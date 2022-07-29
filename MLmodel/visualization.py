5319d2a03be207dd35e20080f83f481cedeb3aab
ProCoderVP <98279986+noobCoderVP@users.noreply.github.com>
2022-07-30 00:04:06 +0530

    Add files via upload
    initial commit

---
+++ MLmodel/visualization.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure(figsize=(8,8))
+
+plt.xlabel("",fontname='Times New Roman',fontsize=20,color='r',weight='bold')
+plt.ylabel("",fontname='Times New Roman',fontsize=20,color='r',weight='bold')
+plt.title("",fontname='Algerian',fontsize=24,color='b',weight='bold')
+plt.legend()
+plt.show()
+# plt.savefig("",dpi=200)
